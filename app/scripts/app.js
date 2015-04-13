8951be45ac2f65d4e99c40e4a6dbf8e0516d0dd3
fishermanswharff <fishermanswharff@github.com>
2015-04-13 11:35:52 -0400

    regenerating app

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name phoneAppApp
+ * @description
+ * # phoneAppApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('phoneAppApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phoneAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phoneAppApp
+ */
+angular.module('phoneAppApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
