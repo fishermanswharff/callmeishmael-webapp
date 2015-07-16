45c891ffd1ca3595aff49f7e522c1ef06e372c7d
fishermanswharff <fishermanswharff@github.com>
2015-07-15 22:19:39 -0400

    Autopush CMI Webapp Master Branch

---
+++ app/scripts/directives/AjaxSpinnerDirective.js
@@ -0,0 +1,15 @@
+'use strict';
+angular.module('MainDirective').directive('ajaxSpinner',['trace','$http',function(trace,$http){
+  return {
+    restrict: 'A',
+    link: function($scope,element,attrs){
+      $scope.isLoading = function(){
+        return $http.pendingRequests.length > 0;
+      };
+
+      $scope.$watch($scope.isLoading,function(v){
+        v ? element.addClass('active').find('i.fa').addClass('fa-cog fa-spin') : element.removeClass('active').find('i.fa').removeClass('fa-cog fa-spin');
+      });
+    }
+  }
+}]);
