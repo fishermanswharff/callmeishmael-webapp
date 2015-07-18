ce85f40cdd5e51ec71647cc393512ef21c6cbda8
fishermanswharff <fishermanswharff@github.com>
2015-07-18 11:24:32 -0400

    new build featuring venue stories

--- app/scripts/controllers/PhoneController.js
+++ app/scripts/controllers/PhoneController.js
@@ -1,11 +1,18 @@
 'use strict';
 angular.module('MainController').controller('PhoneController',phoneController);
-phoneController.$inject = ['$rootScope','AuthFactory','PhoneFactory','$sceDelegate','trace'];
-function phoneController($rootScope,AuthFactory,PhoneFactory,$sceDelegate,trace){
+phoneController.$inject = ['$rootScope','AuthFactory','PhoneFactory','VenueFactory','$sceDelegate','trace'];
+function phoneController($rootScope,AuthFactory,PhoneFactory,VenueFactory,$sceDelegate,trace){
   var vm = this;
   vm.currentPhone = {};
+  vm.venues = []
 
   PhoneFactory.fetch($rootScope.currentUser.venues[0].id).then(function(response){
     angular.copy(response[0], vm.currentPhone);
+  });
+
+  angular.forEach($rootScope.currentUser.venues, function(value,index,array){
+    VenueFactory.fetchOne(value.id).then(function(response){
+      vm.venues.push(response);
+    });
   });
 };
