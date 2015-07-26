b5b81d5053bca603388afc8861140819ee7348ec
fishermanswharff <fishermanswharff@github.com>
2015-07-26 19:32:57 -0400

    Autopush CMI Webapp Master Branch

--- app/scripts/directives/DroppableDirective.js
+++ app/scripts/directives/DroppableDirective.js
@@ -5,48 +5,49 @@ angular.module('MainDirective').directive('cmiDroppable',['$rootScope','trace',f
     scope: {
       button: '='
     },
-    link: function($scope,elem,attrs){
-      $(elem).droppable({
-        drop: function(event,ui) {
-          var newStory,
-              dragId = angular.element(ui.draggable).data('uid'),
-              dragEl = angular.element(ui.draggable).parent(),
-              dropEl = angular.element(this),
-              targetX = $(ui.draggable).offset().left - $(this).offset().left,
-              targetY = $(ui.draggable).offset().top - $(this).offset().top,
-              left = parseInt($(ui.draggable).css('left')) - targetX,
-              top = parseInt($(ui.draggable).css('top')) - targetY,
-              targetW = parseInt($(this).css('width')),
-              targetH = parseInt($(this).css('height'));
+    compile: function(){
+      return function($scope,elem,attrs){
+        $(elem).droppable({
+          drop: function(event,ui) {
+            var newStory,
+                dragId = angular.element(ui.draggable).data('uid'),
+                dragEl = angular.element(ui.draggable).parent(),
+                dropEl = angular.element(this),
+                targetX = $(ui.draggable).offset().left - $(this).offset().left,
+                targetY = $(ui.draggable).offset().top - $(this).offset().top,
+                left = parseInt($(ui.draggable).css('left')) - targetX,
+                top = parseInt($(ui.draggable).css('top')) - targetY,
+                targetW = parseInt($(this).css('width')),
+                targetH = parseInt($(this).css('height'));
 
-          $(ui.draggable).animate({
-            width: targetW,
-            height: targetH,
-            top: top,
-            left: left,
-          }, 100, function(){
-            $(this).fadeOut(400, function(){
-              $(this).remove();
+            $(ui.draggable).animate({
+              width: targetW,
+              height: targetH,
+              top: top,
+              left: left,
+            }, 100, function(){
+              $(this).fadeOut(400, function(){
+                $(this).remove();
+              });
             });
-          });
-
-          newStory = $scope.$parent.phoneController.availableStories.filter(function(value,index,array){
-            if(value.id === dragId){
-              return value;
-            }
-          })[0];
-
-          $rootScope.$broadcast('droppedElement', {
-            dragObj: newStory,
-            dropObj: $scope.button
-          });
-
-          $scope.$apply();
-        },
-        hoverClass: 'cmi-droppable-hover',
-        disabled: '.nonEditable',
-        revert: 'invalid',
-      });
-    },
+            newStory = $scope.$parent.phoneController.availableStories.filter(function(value,index,array){
+              if(value.id === dragId){
+                return value;
+              }
+            })[0];
+            $rootScope.$broadcast('droppedElement', {
+              dragObj: newStory,
+              dropObj: $scope.button
+            });
+            $scope.$apply();
+          },
+          hoverClass: 'cmi-droppable-hover',
+          revert: 'invalid',
+        });
+        setTimeout(function(){
+          $('.nonEditable').droppable( 'option', 'disabled', true );
+        }, 1000);
+      }
+    }
   };
 }]);
