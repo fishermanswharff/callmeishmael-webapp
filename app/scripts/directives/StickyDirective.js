4987dba1d2496e32d447e8fe56e75d4c9422c161
fishermanswharff <fishermanswharff@github.com>
2015-07-29 23:18:04 -0400

    Autopush CMI Webapp Master Branch

---
+++ app/scripts/directives/StickyDirective.js
@@ -0,0 +1,24 @@
+'use strict'
+angular.module('MainDirective').directive('sticky', ['$rootScope','$window','trace',function($rootScope,$window,trace){
+  return {
+    restrict: 'EA',
+    link: function($scope,element,attrs){
+      angular.element($window).bind('scroll',function(){
+        var scrollTop = $window.pageYOffset,
+            elementOffset = $(element).offset().top,
+            parentOffset = $(element).parent().offset().top,
+            distance = (elementOffset - scrollTop),
+            parentDistance = (parentOffset - scrollTop),
+            initialWidth = element.width();
+        if(distance <= 0){
+          $(element).addClass('sticky');
+          element.width(initialWidth);
+        }
+        if(parentDistance > 0) {
+          $(element).removeClass('sticky');
+          element.width('');
+        }
+      })
+    },
+  };
+}]);
