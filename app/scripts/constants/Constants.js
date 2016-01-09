73c3c872bf3e7b4fa5d732944007cc2d6500e3f0
Jason Wharff <fishermanswharff@mac.com>
2016-01-09 13:44:36 -0500

    Autopush CMI Webapp Master Branch

--- app/scripts/constants/Constants.js
+++ app/scripts/constants/Constants.js
@@ -1,5 +1,5 @@
 'use strict';
 angular.module('phoneApp')
   .constant('_', window._)
-  .constant('ServerUrl', 'http://localhost:3333')
+  .constant('ServerUrl', 'https://api.callmeishmael.com')
   .constant('AmazonBucket','https://callmeishmael-files-v2.s3.amazonaws.com/');
