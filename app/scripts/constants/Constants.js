832e16c5e683737c20caceb4b3bc57a7efb402f6
Travis Terry <travisterry@iheartmedia.com>
2016-01-19 02:13:44 -0500

    forgot to reset api endpoint

--- app/scripts/constants/Constants.js
+++ app/scripts/constants/Constants.js
@@ -1,6 +1,6 @@
 'use strict';
 angular.module('phoneApp')
   .constant('_', window._)
-  .constant('ServerUrl', 'http://localhost:3000')
-  //.constant('ServerUrl', 'https://api.callmeishmael.com')
+  //.constant('ServerUrl', 'http://localhost:3000')
+  .constant('ServerUrl', 'https://api.callmeishmael.com')
   .constant('AmazonBucket','https://callmeishmael-files-v2.s3.amazonaws.com/');
